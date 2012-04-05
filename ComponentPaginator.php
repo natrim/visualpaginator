e18d4ddc759fd680c0411cf54707b5fdd94c56ad
Natrim <maly@kinet.cz>
2012-04-05 08:48:51 +0200

    first commit

---
+++ ComponentPaginator.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace VisualPaginator;
+
+use Nette\Application\UI\Control;
+
+
+/**
+ * @author David Grudl
+ * @author Filip Procházka
+ */
+class ComponentPaginator extends Control
+{
+
+    /** @var Paginator */
+    private $paginator;
+
+
+    /**
+     * @return Paginator
+     */
+    public function getPaginator()
+    {
+        if ($this->paginator === NULL) {
+            $this->paginator = new Paginator;
+        }
+
+        return $this->paginator;
+    }
+
+
+    /**
+     * Renders paginator.
+     * @return void
+     */
+    public function render()
+    {
+        echo $this->__toString();
+    }
+
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        $this->template->steps = $this->getPaginator()->getPagesListFriendly();
+        $this->template->paginator = $this->getPaginator();
+
+        if ($this->template->getFile() === NULL) {
+            $this->template->setFile(dirname(__FILE__) . '/template.phtml');
+        }
+
+        return (string)$this->template;
+    }
+
+
+    /**
+     * @param string $page
+     *
+     * @return ComponentPaginator
+     */
+    public function setPage($page)
+    {
+        $this->getPaginator()->page = $page;
+        return $this;
+    }
+
+
+    /**
+     * @param string $file
+     *
+     * @return ComponentPaginator
+     */
+    public function setTemplateFile($file)
+    {
+        $this->getTemplate()->setFile($file);
+        return $this;
+    }
+
+}
---
+++ VisualPaginator.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace VisualPaginator;
+
+
+/**
+ * Visual paginator control.
+ * @author David Grudl
+ * @author Filip Procházka
+ */
+class VisualPaginator extends ComponentPaginator
+{
+
+    /** @persistent */
+    public $page = 1;
+
+
+    /**
+     * Loads state informations.
+     * @param  array
+     * @return void
+     */
+    public function loadState(array $params)
+    {
+        parent::loadState($params);
+        $this->setPage($this->page);
+    }
+
+}
